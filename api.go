844f3cb45ae4f0bfce469e01b749b43063c7486c
Unedo Tampubolon <unedo.tampubolon@gmail.com>
2023-10-03 16:03:21 +0700

    feat : cocurent episode

--- api.go
+++ api.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -18,7 +20,7 @@ func NewAPIClient() (*Apilink, error) {
 	return api_link, nil
 }
 
-func (a *Apilink) SearchAnime(anime_name string) SearchResponse {
+func (a *Apilink) SearchAnime(anime_name string) (error, SearchResponse) {
 
 	url := fmt.Sprintf("%s/anime/gogoanime/%s", a.Api.Url, anime_name)
 	req, err := http.NewRequest("GET", url, nil)
@@ -29,48 +31,52 @@ func (a *Apilink) SearchAnime(anime_name string) SearchResponse {
 	client := http.DefaultClient
 	res, err := client.Do(req)
 	if err != nil {
-		log.Fatalf("%v", err)
+		return err, SearchResponse{}
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
-		panic("Api Link Is Not Valid")
+		return errors.New("status not ok"), SearchResponse{}
 	}
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		log.Fatalf("%v", err)
+		return err, SearchResponse{}
 	}
 	var res_body SearchResponse
 	err = json.Unmarshal(body, &res_body)
 	if err != nil {
-		log.Fatalf("%v", err)
+		return err, SearchResponse{}
 	}
-	return res_body
+	return nil, res_body
 }
 
 func (a *Apilink) GetVideoLink(id string) map[string]string {
 	var video_url VideoUrl
+	quality_link := make(map[string]string)
+
 	url := fmt.Sprintf("%s/anime/gogoanime/watch/%s", a.Api.Url, id)
 	req, err := http.NewRequest("GET", url, nil)
-	ctx, cencel := context.WithTimeout(context.Background(), time.Second*3)
+	ctx, cencel := context.WithTimeout(context.Background(), time.Second*5)
 	req = req.WithContext(ctx)
 	defer cencel()
 
 	client := http.DefaultClient
 	res, err := client.Do(req)
 	if err != nil {
-		log.Fatalf("%v", err)
+		// log.Printf("%v", err)
+		return nil
 	}
 	defer res.Body.Close()
 	if res.StatusCode != http.StatusOK {
-		log.Fatalf("%v", err)
+		log.Printf("%v", err)
+		return nil
 	}
 	defer res.Body.Close()
 	data, err := io.ReadAll(res.Body)
 	if err := json.Unmarshal(data, &video_url); err != nil {
-		log.Fatalf("%v", err)
+		log.Printf("%v", err)
+		return nil
 	}
-	quality_link := make(map[string]string)
 	lenght_arr := len(video_url.Sources)
 	for i := lenght_arr - 3; i >= 0; i-- {
 		quality_link[video_url.Sources[i].Quality] = video_url.Sources[i].Url
@@ -82,31 +88,59 @@ func (a *Apilink) GetTotalEpisode(id string) int {
 	var AnimeInfo map[string]interface{}
 	url := fmt.Sprintf("%s/anime/gogoanime/info/%s", a.Api.Url, id)
 	req, err := http.NewRequest("GET", url, nil)
-	ctx, cencel := context.WithTimeout(context.Background(), time.Second*3)
+	ctx, cencel := context.WithTimeout(context.Background(), time.Second*5)
 	req = req.WithContext(ctx)
 	defer cencel()
 
 	client := http.DefaultClient
 	res, err := client.Do(req)
 	if err != nil {
-		log.Fatalf("%v", err)
+		log.Printf("%v", err)
+		return 0
 	}
 	defer res.Body.Close()
 	data, err := io.ReadAll(res.Body)
 	if err != nil {
-		log.Fatalf("%v", err)
+		log.Printf("%v", err)
+		return 0
 	}
 	if err := json.Unmarshal(data, &AnimeInfo); err != nil {
-		log.Fatalf("%v", err)
+		log.Printf("%v", err)
+		return 0
 	}
 	totalEpisode, ok := AnimeInfo["totalEpisodes"].(float64)
 	if !ok {
-		panic("Episode Not Found")
+		return 0
 	}
 	return int(totalEpisode)
 }
 
 func (a *Apilink) GetEpisodeUrl(id string) map[string]string {
-	video_url := a.GetVideoLink(id)
-	return video_url
+	videochan := make(chan map[string]string)
+	go func() {
+		for {
+			video_url := a.GetVideoLink(id)
+			if video_url == nil {
+				continue
+			}
+			videochan <- video_url
+		}
+	}()
+	return <-videochan
+}
+
+func (a *Apilink) ConcurentEpisode(total_e int, e_id map[int]string, eps_url *map[string]map[string]string) {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	wg.Add(total_e)
+	for i := 1; i <= total_e; i++ {
+		go func(i int) {
+			video_url := a.GetEpisodeUrl(e_id[i])
+			mu.Lock()
+			(*eps_url)[e_id[i]] = video_url
+			mu.Unlock()
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
 }
